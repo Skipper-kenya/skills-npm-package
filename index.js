eb5f17fd9aa2738ffab518dd97ae2bdee8ae50c1
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-02-26 19:42:56 +0300

    readme update

---
+++ index.js
@@ -0,0 +1,11 @@
+const skills = () => {
+  return [
+    { Frontend: ["HMTL5", "CSS", "JavaScript", "React"] },
+    { Backend: ["Node", "Express", "socket"] },
+    { MaterialUI: ["ant-design"] },
+    { StateManagement: ["redux", "contextApi"] },
+    { Database: ["MongoDB", "MYSQL"] },
+  ];
+};
+
+module.exports = skills;
